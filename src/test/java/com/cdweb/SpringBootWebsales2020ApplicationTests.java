eb96ee5d6a8a77e18e075c821959045ac5ab912b
Phung <19130175@st.hcmuaf.edu.vn>
2023-05-24 16:00:26 +0700

    upload

--- src/test/java/com/cdweb/SpringBootWebsales2020ApplicationTests.java
+++ src/test/java/com/cdweb/SpringBootWebsales2020ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootWebsales2020ApplicationTests {
+class SpringBootWebTests {
 
 	@Test
 	void contextLoads() {
